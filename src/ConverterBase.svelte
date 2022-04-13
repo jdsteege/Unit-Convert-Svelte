50f22661ce3b9ebcd5c9a9cc512812ab8267e831
jdsteege <jdsteege@gmail.com>
2022-04-13 13:14:15 -0500

    Initial setup.

--- src/ConverterBase.svelte
+++ src/ConverterBase.svelte
@@ -1,23 +1,19 @@
 <script>
-	import QualitySelect from './QualitySelect.svelte';
-	import ScaleSelect from './ScaleSelect.svelte';
+  import QualitySelect from "./QualitySelect.svelte";
+  import ScaleSelect from "./ScaleSelect.svelte";
 </script>
 
-<h1>
-	ConverterBase
-</h1>
+<h1>ConverterBase</h1>
 
 <ion-content>
-	<ion-grid>
-	<ion-row>
-		<ion-col size=1/>
-		<ion-col>
-
-<QualitySelect/>
-<ScaleSelect/>
-			
-			</ion-col>
-		<ion-col size=1/>
-		</ion-row>
-	</ion-grid>
+  <ion-grid>
+    <ion-row>
+      <ion-col size="1" />
+      <ion-col>
+        <QualitySelect />
+        <ScaleSelect />
+      </ion-col>
+      <ion-col size="1" />
+    </ion-row>
+  </ion-grid>
 </ion-content>
